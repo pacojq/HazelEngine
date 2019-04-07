4b39f57247b38ad66b8a10e7edbf0601efdd0b4f
PJ <pacojuanquiros@gmail.com>
2019-04-07 14:08:20 +0200

    Added Glad to the solution

--- Hazel/src/Hazel/Layer.h
+++ Hazel/src/Hazel/Layer.h
@@ -17,7 +17,7 @@ namespace Hazel {
 		virtual void OnUpdate() {}
 		virtual void OnEvent(Event& event) {}
 
-		inline const std::string& GetNAme() const { return m_DebugName; }
+		inline const std::string& GetName() const { return m_DebugName; }
 
 	protected:
 		std::string m_DebugName;
